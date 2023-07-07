abf1fccd3b048c553bd10477b65db03ae9d1b2c0
Alvydas Vitkauskas <alvydas@ondato.com>
2023-07-07 13:21:29 +0300

    day 06

---
+++ src/aoc/d06_2.clj
@@ -0,0 +1,19 @@
+(ns aoc.d06-2
+  (:require
+    [clojure.string :as s]))
+
+(defn get-columns []
+  (->> (slurp "data/input06.txt")
+       (s/split-lines)
+       (apply map vector)))
+
+(defn -main []
+  (->> (get-columns)
+       (map frequencies)
+       (map #(sort-by val %))
+       (map #((comp first first) %))
+       (apply str)))
+
+(comment
+  (-main))
+  
